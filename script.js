f2e98718a42d9fa34a182dbd607ed0f0e753a4ed
karanxhagiulia <karanxhagiulia@gmail.com>
2024-04-14 21:30:28 +0200

    update

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const housesContainer = document.querySelector('.houses-container'); // Select the container of house icons
-    const houses = housesContainer.querySelectorAll('.house-animate'); // Select all house icons
+    const scrollElements = document.querySelectorAll('.scroll-animate'); // Select elements to animate
 
     window.addEventListener('scroll', function() {
-        houses.forEach(house => {
-            if (isScrolledIntoView(house) && !house.classList.contains('fadeIn')) {
-                house.classList.add('fadeIn'); // Apply the 'fadeIn' class to the house icon
+        scrollElements.forEach(element => {
+            if (isScrolledIntoView(element) && !element.classList.contains('animate-scroll')) {
+                element.classList.add('animate-scroll'); // Apply the 'animate-scroll' class to trigger animation
             }
         });
     });
@@ -27,5 +26,25 @@ document.getElementById("showChartsButton").addEventListener("click", function()
         chartContainer.style.display = "block";
     } else {
         chartContainer.style.display = "none";
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const housingContainer = document.querySelector('.houses-container'); // Select housing container to animate
+
+    window.addEventListener('scroll', function() {
+        if (isScrolledIntoView(housingContainer) && !housingContainer.classList.contains('animate-scroll')) {
+            housingContainer.classList.add('animate-scroll'); // Apply the 'animate-scroll' class to trigger animation
+        }
+    });
+
+    function isScrolledIntoView(element) {
+        const rect = element.getBoundingClientRect();
+        const elemTop = rect.top;
+        const elemBottom = rect.bottom;
+
+        // Only completely visible elements return true:
+        const isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+        return isVisible;
     }
 });
